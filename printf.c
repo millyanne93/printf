737904c1a47562bd704c84418c4363d1fb7799cd
Bolo Denis <bollodenis@gmail.com>
2023-08-20 08:54:39 -0700

    printf.c

--- printf.c
+++ printf.c
@@ -1,44 +1,43 @@
 #include "main.h"
 #include <string.h>
+
 /**
- *_printf - print formatted text to stdout
- *@format: format specifiers of printf function
- *return: total no of characters printed
+ * _printf - print formatted text to stdout
+ * @format: format specifiers for the printf function
+ * Return: total number of characters printed
  */
 int _printf(const char *format, ...)
-{	 int i, st_count;
-	int total_count = 0;
-	va_list git;
-
-	va_start(git, format);
-	if (format[i] =="%" && format[i] == '\0')
-	{
-		return -1;
-	}
-	for (i = 0; format[i] != '\0'; i++)
 {
-	{	if (format[i] != '%')
-		{
-		_pputchar(format[i]);
-		}	
-		else if (format[i + 1] == 'c')
-		{
-			_pputchar(va_arg(git, int));
-			i++;
-		}
-		else if (format[i + 1] == 's')
-		{
-			st_count = _puts(va_arg(git, char *));
-			i++;
-		total_count += (st_count - 1);
-		}
-		else if (format[i + 1] == '%')
-		{
-			_pputchar ('%');
-		}
-		total_count = +1;
+int i, st_count;
+int total_count = 0;
+va_list git;
+va_start(git, format);
+for (i = 0; format[i] != '\0'; i++)
+{
+if (format[i] != '%')
+{
+_pputchar(format[i]);
+total_count++;
 }
-		va_end(git);
-		return (total_count);
+else if (format[i + 1] == 'c')
+{
+_pputchar(va_arg(git, int));
+total_count++;
+i++;
 }
-
+else if (format[i + 1] == 's')
+{
+st_count = _puts(va_arg(git, char *));
+total_count += st_count;
+i++;
+}
+else if (format[i + 1] == '%')
+{
+_pputchar('%');
+total_count++;
+i++;
+}
+}
+va_end(git);
+return (total_count);
+}
