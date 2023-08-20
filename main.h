2f1ecc2b67566ac95bf7f892d7c0a5129bcfe452
Bolo Denis <bollodenis@gmail.com>
2023-08-20 07:50:03 -0700

    main.h

--- main.h
+++ main.h
@@ -6,6 +6,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * struct _specifier- Struct specifier
+ *
+ * @_printf: format outputs pinted text in stdout
+ * @f: The function associated
+ */
+typedef struct specifier
+{
+    char *str;
+    int (*f)(int a, int b);
+}spec;
+
 int _printf(const char *format, ...);
 int _pputchar(char c);
 int _puts(const char *str);
