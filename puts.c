666e4b3cd6820e899a25d4c633bee621e4feef0b
Bolo Denis <bollodenis@gmail.com>
2023-08-20 04:53:42 -0700

    puts

---
+++ puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - writes strings to stdout
+ * @string: character array of strings passed to the function
+ * Return: number of characters written (excluding the null-terminator)
+ */
+int _puts(const char *str)
+{
+    int count = 0;
+
+    if (str != NULL)
+    {
+        for (count = 0; count[str] != '\0'; count ++)
+        {
+            _pputchar(str[count]);
+        }
+       _pputchar('\n'); 
+    }
+
+    return count;
+}
