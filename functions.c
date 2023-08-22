1e28639acfdc49474616358ea9978676e35f5887
millyanne93 <nmillyanne20@gmail.com>
2023-08-22 01:59:15 -0700

    made changes

--- functions.c
+++ functions.c
@@ -1,14 +1,17 @@
 #include "main.h"
+
 /**
- * _string - returns number of counts in a string
- * @args: the input argument
- * Return: count
+ * _string - prints a string
+ * @git: argument list
+ * Return: number of characters printed
  */
 int _string(va_list git)
 {
 	int count = 0;
-
 	char *mil = va_arg(git, char *);
+
+	if (str == NULL)
+		return ("nil");
 
 	while (*mil != '\0')
 	{
@@ -20,9 +23,9 @@ int _string(va_list git)
 }
 
 /**
- * _char - return number of bytes
- * @args: the input argument
- * Return: count
+ * _char - prints a character
+ * @git: argument list
+ * Return: number of characters printed
  */
 int _char(va_list git)
 {
@@ -33,8 +36,8 @@ int _char(va_list git)
 }
 
 /**
- * _percent - print percent sign
- * @args: the input argument(unused, can be removed)
+ * _percent - prints a percent sign
+ * @git: argument list (unused attribute)
  * Return: 1 (number of characters printed)
  */
 int _percent(va_list git __attribute__((unused)))
@@ -43,4 +46,32 @@ int _percent(va_list git __attribute__((unused)))
 
 	_pputchar(*mil);
 	return (1);
+}
+
+/**
+ * _decimal - prints decimal numbers
+ * @git: argument list
+ * Return: count
+ */
+int _decimal(va_list git)
+{
+	int count = 0;
+	int mil = va_arg(git, int);
+
+	count = _pputchar(mil, 0);
+	return (count);
+}
+
+/**
+ * _integer - prints an integer
+ * @git: argument list
+ * Return: count
+ */
+int _integer(va_list git)
+{
+	int count = 0;
+	int mil = va_arg(git, int);
+
+	count = _pputchar(mil, 0);
+	return (count);
 }
